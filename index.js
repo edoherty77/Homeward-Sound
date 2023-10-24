5a1cd618208e1aff16e9bbe76b65825aee11c21c
evan doherty <edoherty77@gmail.com>
2023-10-23 21:03:18 -0400

    musicians data set up

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-alert("hello");
+let musiciansContainer = document.getElementById("musiciansContainer");
+
+for (let i = 0; i < musiciansData.length; i++) {
+  let musicianDiv = document.createElement("div");
+  musicianDiv.classList.add("musicianDiv");
+  musiciansContainer.appendChild(musicianDiv);
+
+  let nameDiv = document.createElement("div");
+  nameDiv.innerHTML = musiciansData[i].name;
+  musicianDiv.appendChild(nameDiv);
+
+  let ageDiv = document.createElement("div");
+  ageDiv.innerHTML = musiciansData[i].age;
+  musicianDiv.appendChild(ageDiv);
+
+  let bioDiv = document.createElement("div");
+  bioDiv.innerHTML = musiciansData[i].bio;
+  musicianDiv.appendChild(bioDiv);
+}
